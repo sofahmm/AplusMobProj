2cacd08fb6706a92eb209cc28a5e5df9d746c7c1
sofahmm <74815523+sofahmm@users.noreply.github.com>
2022-02-10 19:42:44 +0300

    add editor

---
+++ AplusMobProj/AplusMobProj/Pages/EditProjectPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AplusMobProj.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditProjectPage : ContentPage
+    {
+        public EditProjectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
